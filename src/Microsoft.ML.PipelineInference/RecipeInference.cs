d245039f5f3d658ba99b3a0c5bf1cb8ecf3dbc51
Daniel Holstein <daholste@microsoft.com>
2018-11-18 23:47:14 -0800

    blacklist some broken (Rsquared far less than 0?!?) and low performing algos

--- src/Microsoft.ML.PipelineInference/RecipeInference.cs
+++ src/Microsoft.ML.PipelineInference/RecipeInference.cs
@@ -559,6 +559,9 @@ namespace Microsoft.ML.Runtime.PipelineInference
                 .Where(p => type.IsAssignableFrom(p) &&
                     MacroUtils.IsTrainerOfKind(p, trainerKind));
             trainerTypes = trainerTypes.Where(y => !typeof(GeneralizedAdditiveModelBinaryClassifier).Equals(y));
+            trainerTypes = trainerTypes.Where(y => !typeof(EnsembleBinaryClassifier).Equals(y));
+            trainerTypes = trainerTypes.Where(y => !typeof(OnlineGradientDescentRegressor).Equals(y));
+            trainerTypes = trainerTypes.Where(y => !typeof(FastTreeTweedieRegressor).Equals(y));
 
             foreach (var tt in trainerTypes)
             {
